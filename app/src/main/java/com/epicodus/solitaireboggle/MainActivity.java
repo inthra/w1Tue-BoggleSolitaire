6913d9635e50b72cc90877624fc1d978ea26866d
Inthrayuth Mahaphol and Nick Lyman <nick@>
2016-06-28 14:46:40 -0700

    Create random letter output using do/while loop with nested for loop to make sure there are at least 2 vowels within the 8 letter output. Print to screen and style with character spacing and adjust font size.

--- app/src/main/java/com/epicodus/solitaireboggle/MainActivity.java
+++ app/src/main/java/com/epicodus/solitaireboggle/MainActivity.java
@@ -2,9 +2,17 @@ package com.epicodus.solitaireboggle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mGenerateBoggleLettersButton;
+    private TextView mShowLetters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,17 +20,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mGenerateBoggleLettersButton = (Button) findViewById(R.id.playBoggle);
+        mShowLetters = (TextView) findViewById(R.id.showLetters);
         mGenerateBoggleLettersButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick() {
+            public void onClick(View view) {
                 String setOfCharacters = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
                 String boggleLetters = "";
+                int countVowels = 0;
 
-                for (int i = 0; i <= 7; i++) {
-                    boggleLetters += setOfCharacters.charAt(new Random().nextInt(setOfCharacters.length()));
-                }
-                log.d(TAG, boggleLetters);
+                do {
+                    boggleLetters = "";
+                    for (int i = 0; i <= 7; i++) {
+                        boggleLetters += setOfCharacters.charAt(new Random().nextInt(setOfCharacters.length()));
+                    }
+                    Log.d(TAG, boggleLetters);
+
+                    countVowels = 0;
+                    for (int i =0; i <= boggleLetters.length() - 1; i++) {
+                        char checkEachLetter = boggleLetters.charAt(i);
+                        if (checkEachLetter == 'A' || checkEachLetter == 'E' || checkEachLetter == 'I' || checkEachLetter == 'O' || checkEachLetter == 'U') {
+                            countVowels++;
+                        }
+                    }
+                } while (countVowels < 2);
+
+                mShowLetters.setText(boggleLetters);
             }
         });
     }
