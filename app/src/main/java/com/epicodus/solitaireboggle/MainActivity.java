6ac32df3705121fb40c481ffd297ee0c45983a13
Inthrayuth Mahaphol and Nick Lyman <nick@>
2016-06-28 15:16:21 -0700

    Add ability to getText when user input a word, but not finish the checking the word with the generated letters yet.

--- app/src/main/java/com/epicodus/solitaireboggle/MainActivity.java
+++ app/src/main/java/com/epicodus/solitaireboggle/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -12,7 +13,9 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mGenerateBoggleLettersButton;
+    private Button mCheckWord;
     private TextView mShowLetters;
+    private EditText mUserInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,31 +24,42 @@ public class MainActivity extends AppCompatActivity {
 
         mGenerateBoggleLettersButton = (Button) findViewById(R.id.playBoggle);
         mShowLetters = (TextView) findViewById(R.id.showLetters);
+
+        mCheckWord = (Button) findViewById(R.id.checkWord);
+        mUserInput = (EditText) findViewById(R.id.userInput);
+
         mGenerateBoggleLettersButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                String setOfCharacters = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
+                String setOfCharacters = "abcdefghijklmnopqrstuvwxyz";
                 String boggleLetters = "";
                 int countVowels = 0;
 
                 do {
                     boggleLetters = "";
                     for (int i = 0; i <= 7; i++) {
-                        boggleLetters += setOfCharacters.charAt(new Random().nextInt(setOfCharacters.length()));
+                        boggleLetters += setOfCharacters.charAt(new Random().nextInt(setOfCharacters.length()-1));
                     }
-                    Log.d(TAG, boggleLetters);
 
                     countVowels = 0;
                     for (int i =0; i <= boggleLetters.length() - 1; i++) {
                         char checkEachLetter = boggleLetters.charAt(i);
-                        if (checkEachLetter == 'A' || checkEachLetter == 'E' || checkEachLetter == 'I' || checkEachLetter == 'O' || checkEachLetter == 'U') {
+                        if (checkEachLetter == 'a' || checkEachLetter == 'e' || checkEachLetter == 'i' || checkEachLetter == 'o' || checkEachLetter == 'u') {
                             countVowels++;
                         }
                     }
                 } while (countVowels < 2);
 
                 mShowLetters.setText(boggleLetters);
+            }
+        });
+
+        mCheckWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String boggleWord = mUserInput.getText().toString();
+                Log.d(TAG, boggleWord);
             }
         });
     }
